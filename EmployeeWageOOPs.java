fd1ff83e4c84e923428975ce892d9a6ee8509995
Tejal Gaikwad <you@example.com>
2022-09-15 16:55:20 +0530

    Added : code Calculate daily emp wage

--- EmployeeWageOOPs.java
+++ EmployeeWageOOPs.java
@@ -1,20 +1,27 @@
 
 public class EmployeeWageOOPs {
 
-	public static final int IsFullTime = 1;
-	public static final int IsPartTime = 2;
+	public static final int Emp_present = 1;
+	public static final int Wage_per_hour = 20;
 
-	public void attendance() {
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IsFullTime) {
-			System.out.println("Employee is Present");
+	public void dailyWage() {
+
+		double empcheck = Math.floor(Math.random() * 10) % 2;
+		if (empcheck == Emp_present) {
+			System.out.println("Employee is present");
+			int Full_day_hour = 8;
+			int Salary = (Wage_per_hour * Full_day_hour);
+			System.out.println("Total_Salary" + Salary);
 		} else {
-			System.out.println("Employee is Not Present");
+			System.out.println("Employee is not present");
+			int Full_day_hour = 0;
+			int Salary = (Wage_per_hour * Full_day_hour);
+			System.out.println("Salary is" + Salary);
 		}
 	}
 
 	public static void main(String[] args) {
 		EmployeeWageOOPs e1 = new EmployeeWageOOPs();
-		e1.attendance();
+		e1.dailyWage();
 	}
 }
